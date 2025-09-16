cd9cb9e1d5ce8b97bcd5ceed71517decb7dc88d6
Felix Gateru <felix.gateru@gmail.com>
2025-09-16 18:21:39 +0300

    SMQ-2944 - Add client events consumer (#2947)
    Signed-off-by: Felix Gateru <felix.gateru@gmail.com>

---
+++ pkg/clients/events/doc.go
@@ -0,0 +1,6 @@
+// Copyright (c) Abstract Machines
+// SPDX-License-Identifier: Apache-2.0
+
+// Package events provides the events sourcing of clients to
+// provide replication in other service and definitions needed to support it
+package events
